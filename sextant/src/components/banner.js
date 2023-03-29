e173c14e752e149c775bf460eb58aa34d52c8eb7
shensim94 <shensim@oregonstate.edu>
2023-03-29 00:42:34 -0700

    added banner

---
+++ sextant/src/components/banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner(){
+    return(
+        <div className = "banner" style={{backgroundColor:'#f0f0f0'}}>
+            <p className = "banner-text">Cisco Sextant</p>
+        </div>
+    )
+}
+
+export default Banner
